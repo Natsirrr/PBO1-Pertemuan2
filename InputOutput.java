001461dbdd0f88da372c9b3d5347b76ac69b0013
Natsirrr <m.natsirboy@gmail.com>
2021-04-02 17:27:16 +0800

    Memodifikasi File Input Output

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,26 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
-  System.out.print("Masukkan nama anda :");
+  String ttl;
+  String sp;
+  String gd;
+
+  System.out.println();
+  System.out.print("Masukkan Nama Anda : ");
   nama = scanner.nextLine();
-  System.out.println("Hai, "+nama);
+  System.out.print("Masukkan Tempat Tanggal Lahir : ");
+  ttl = scanner.nextLine();
+  System.out.print("Masukkan Status Perkawinan : ");
+  sp = scanner.nextLine();
+  System.out.print("Masukkan Golongan Darah : ");
+  gd = scanner.nextLine();
+
+  System.out.println();
+  System.out.println("==================================================");
+  System.out.println("Nama : "+nama);
+  System.out.println("Tempat Tanggal Lahir : "+ttl);
+  System.out.println("Status Perkawinan : "+sp);
+  System.out.println("Golongan Darah : "+gd);
+  System.out.println("==================================================");
  }
 }
